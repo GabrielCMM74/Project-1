2a77365ec27d624b79bd909635e89a86c23c761c
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-03-21 13:36:55 -0400

    Reset-Btn

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
 console.log(`Main JS File is Loaded`)
 
 // State Variables 
-let currentBalance = 1000;
+let currentBalance;
 let currentBet;
 let currentSelection;
 let currentWinner;
 
 let resetGame = document.getElementById('resetwheel');
-let spinWheel = document.getElementById('spinwheel');
+let spinBtn = document.getElementById('spinwheel');
+let wheelResult = document.getElementById('wheel')
 
 // Event Listeners
 document.querySelector('.angry-grid').addEventListener('click', handleClick);
 document.getElementById('placebet').addEventListener('click', handleClick);
 document.querySelector('.angry-grid').addEventListener('click', handleClick);
 resetGame.addEventListener('click', handleClick);
-spinWheel.addEventListener('click', handleClick);
+spinwheel.addEventListener('click', getNumber);
+resetGame.addEventListener('click', reset);
 
 
-// init();
+init();
 
-// function init(){
-//     currentBalance = 1000;
-//     currentBet = enterbet.value;
-//     statusmessage.innerHTML = `Place your bets!!!`;
-//     statusmessage.style.backgroundColor = 'black';
-//     statusmessage.style.color = 'gold';
-//     enterbet.value = '';
-//     wheelResult.innerHTML = '';
-//     render();
-// };
+function init(){
+    currentBalance = 1000;
+    currentBet = enterbet.value;
+    statusmessage.innerHTML = `Place your bets!!!`;
+    statusmessage.style.backgroundColor = 'black';
+    statusmessage.style.color = 'gold';
+    enterbet.value = '';
+    wheelResult.innerHTML = '';
+    render();
+};
 
-// init()
+
 
 function adjustBalance(){
     if (currentBalance > 0 && currentBalance >= parseInt(enterbet.value)){
@@ -50,6 +52,23 @@ function adjustBalance(){
         }
         render();
     }
+    if (currentSelection){
+        currentSelection.style.border = 'none';
+        currentSelection = undefined;
+    }
+};
+
+
+
+function getNumber(){
+    let wheelValue = (Math.floor(Math.random() * 36 + 1));
+    wheelResult.innerHTML = wheelValue;
+    console.log(`YOU WON`)
+};
+getNumber()
+
+function reset(){
+    init();
     if (currentSelection){
         currentSelection.style.border = 'none';
         currentSelection = undefined;
