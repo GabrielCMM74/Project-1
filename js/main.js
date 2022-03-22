4fe26b29ba5fedfe43e4195ab4edcce4cf50e1ed
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-03-21 20:06:42 -0400

    More win scenarios

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ console.log(`Main JS File is Loaded`)
 // All my Arrays 
 let ifEven = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36];
 let ifOdd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35];
-let ifRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-let ifBlack = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 20, 31, 33, 35];
+let ifRed = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35];
+let ifBlack = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36];
 let ifFirst12 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let ifSecond12 = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
 let ifThird12 = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
@@ -35,6 +35,10 @@ document.getElementById('Second-12').addEventListener('click', winSecond12)
 document.getElementById('Third-12').addEventListener('click', winThird12)
 document.getElementById('Even-W').addEventListener('click', winEven)
 document.getElementById('Odd-W').addEventListener('click', winOdd)
+document.getElementById('Black-W').addEventListener('click', winBlack)
+document.getElementById('Red-W').addEventListener('click', winRed)
+document.getElementById('OneEight-W').addEventListener('click', winOneEighteen)
+document.getElementById('NineThirty-W').addEventListener('click', winNineThirtySix)
 
 init();
 
@@ -91,31 +95,52 @@ function getNumber(){
 
 /////// 1-12 SCENARIOS/////////
 function winFirst12(e){
-    currentSelection = ifFirst12};
+currentSelection = ifFirst12};
 
 function winSecond12(e){
-   currentSelection = ifSecond12};
+currentSelection = ifSecond12};
 
 function winThird12(e){
-     currentSelection = ifThird12};
+currentSelection = ifThird12};
 
 ////// EVEN / ODD SCENARIOS//////
 
 function winEven(e){
-    currentSelection = ifEven};
+currentSelection = ifEven};
 
 function winOdd(e){
-    currentSelection = ifOdd};
+currentSelection = ifOdd};
 
 ////////// RED / BLACK SCENARIOS////////
 
 function winRed(e){
-    currentSelection = ifRed};
+currentSelection = ifRed};
 
 function winBlack(e){
-    currentSelection = ifBlack};
+currentSelection = ifBlack};
 
 /////// 1-18 19-36 SCENARIOS///////
+
+function winNineThirtySix(e){
+currentSelection = ifNinetThirty};
+
+function winOneEighteen(e){
+currentSelection = ifOnetEight};
+
+////////// ROW SCENARIOS//////////
+
+function winFirstRow(e){
+currentSelection = ifFirstRow};
+    
+function winSecondRow(e){
+currentSelection = ifSecondRow};
+
+function winThirdRow(e){
+currentSelection = ifThirdRow};
+
+////////////// Numbers //////////
+
+
 function reset(){
     init();
     
